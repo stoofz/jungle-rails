7cc8da58cd3da7ec046d727467173667415a66ce
Anthony Chambers <ajfchambers@gmail.com>
2023-08-14 15:42:04 -0400

    Add cart test

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -7,14 +7,15 @@ describe('Home', () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
-
+  
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
   });
   
-  it('Visit a product detail page', () => {
-    cy.get(".products article").first().click();
-    cy.url().should("include", "/products/2");
+  it('Add product to cart, which increase cart to 1', () => {
+    cy.contains('My Cart (0)');
+    cy.contains('Add').first().click({ force: true });
+    cy.contains('My Cart (1)');
   });
 
 });
